47dd321c0b7bca08119e4d36bab310c8ea128386
CarlosMelgar <164534523+Carlos-Melgar-1079@users.noreply.github.com>
2024-11-11 14:12:11 -0600

    Add files via upload

--- UIII_p1_CRUD_tablas_Materiav2/materia_app/views.py
+++ UIII_p1_CRUD_tablas_Materiav2/materia_app/views.py
@@ -12,8 +12,26 @@ def registrarMateria(request):
     codigo=request.POST["txtcodigo"]
     nombre=request.POST["txtnombre"]
     creditos=request.POST["numcreditos"]
-
-
     guardarmateria=Materia.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+    return redirect("/")
+
+def seleccionarMateria(request,codigo):
+    materia=Materia.objects.get(codigo=codigo)
+    return render(request,"editarmateria.html",{"mismaterias":materia})
+
+
+def editarMateria(request):
+    codigo=request.POST["txtcodigo"]
+    nombre=request.POST["txtnombre"]
+    creditos=request.POST["numcreditos"]
+    materia=Materia.objects.get(codigo=codigo)
+    materia.nombre=nombre
+    materia.creditos=creditos
+    materia.save()
+    return redirect("/")
+
+def borrarMateria(request,codigo):
+    materia=Materia.objects.get(codigo=codigo)
+    materia.delete()
 
     return redirect("/")
